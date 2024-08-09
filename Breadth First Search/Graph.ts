86e18857a28dd286bcdd73372f4efb59d5e4ca35
Mohamed Lasheen <mohamedlasheen637@gmail.com>
2024-08-09 21:16:08 +0300

    ADD breadth first search

--- Breadth First Search/Graph.ts
+++ Breadth First Search/Graph.ts
@@ -1,3 +1,4 @@
+import { Queue } from "queue-typed";
 import Edge from "./Edge";
 import Vertex from "./Vertex";
 
@@ -19,7 +20,39 @@ class Graph {
       this.vertices[sourceIndex].vertexLinks[i] = new Edge(
         this.vertices[sourceIndex],
         this.vertices[targetsIndices[i]]
-      )
+      );
+    }
+  }
+  public bfs() {
+    console.log("Breadth First Search from Graph Class");
+    const queue = new Queue<Vertex>();
+
+    queue.push(this.vertices[0]);
+    this.vertices[0].visited = true;
+
+    let destinations: Edge[];
+    let currentVertex: Vertex;
+
+    while (!queue.isEmpty()) {
+      currentVertex = queue.shift()!;
+      destinations = currentVertex!.vertexLinks;
+
+      for (let i = 0; i < destinations.length; i++) {
+        if (destinations[i].target.visited === false) {
+          queue.push(destinations[i].target);
+          destinations[i].target.visited = true;
+
+          console.log(
+            `${currentVertex.name} -> ${destinations[i].target.name}`
+          );
+        }
+      }
+    }
+  }
+  //If I use this Graph for other purposes
+  public resetVertices() {
+    for (const vertex of this.vertices) {
+      vertex.visited = false;
     }
   }
 }
--- index.ts
+++ index.ts
@@ -1,6 +1,17 @@
+import Graph from "./Breadth First Search/Graph";
 
+const graph = new Graph(["A", "B", "C", "D", "E", "F", "G", "H", "I"]);
+graph.addEdges(0, [1, 2]);
+graph.addEdges(1, [0, 3, 4]);
+graph.addEdges(2, [0, 3, 5]);
+graph.addEdges(3, [1, 2, 4]);
+graph.addEdges(4, [1, 5]);
+graph.addEdges(5, [2, 3, 4, 7]);
+graph.addEdges(6, [7, 8]);
+graph.addEdges(7, [5, 6, 8]);
+graph.addEdges(8, [6, 7]);
 
-
+graph.bfs();
 
 /*
 console.log('abc'.charCodeAt(2));
